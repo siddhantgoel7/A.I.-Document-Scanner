8a0b066297457a5c9274aead3bad9aa180700a00
siddhantgoel7 <89402613+siddhantgoel7@users.noreply.github.com>
2022-08-09 15:02:08 +0530

    Update documentscanner.py

--- documentscanner.py
+++ documentscanner.py
@@ -1,67 +1,118 @@
 import cv2
 import numpy as np
-frameWidth = 640
-frameHeight = 480
+
+
+###################################
+widthImg=540
+heightImg =640
+#####################################
+
 cap = cv2.VideoCapture(1)
-cap.set(3, frameWidth)
-cap.set(4, frameHeight)
 cap.set(10,150)
 
-myColors = [[5,107,0,19,255,255],
-            [133,56,0,159,156,255],
-            [57,76,0,100,255,255],
-            [90,48,0,118,255,255]]
-myColorValues = [[51,153,255],          ## BGR
-                 [255,0,255],
-                 [0,255,0],
-                 [255,0,0]]
 
-myPoints =  []  ## [x , y , colorId ]
-
-def findColor(img,myColors,myColorValues):
-    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    count = 0
-    newPoints=[]
-    for color in myColors:
-        lower = np.array(color[0:3])
-        upper = np.array(color[3:6])
-        mask = cv2.inRange(imgHSV,lower,upper)
-        x,y=getContours(mask)
-        cv2.circle(imgResult,(x,y),15,myColorValues[count],cv2.FILLED)
-        if x!=0 and y!=0:
-            newPoints.append([x,y,count])
-        count +=1
-        #cv2.imshow(str(color[0]),mask)
-    return newPoints
+def preProcessing(img):
+    imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    imgBlur = cv2.GaussianBlur(imgGray,(5,5),1)
+    imgCanny = cv2.Canny(imgBlur,200,200)
+    kernel = np.ones((5,5))
+    imgDial = cv2.dilate(imgCanny,kernel,iterations=2)
+    imgThres = cv2.erode(imgDial,kernel,iterations=1)
+    return imgThres
 
 def getContours(img):
+    biggest = np.array([])
+    maxArea = 0
     contours,hierarchy = cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
-    x,y,w,h = 0,0,0,0
     for cnt in contours:
         area = cv2.contourArea(cnt)
-        if area>500:
-            #cv2.drawContours(imgResult, cnt, -1, (255, 0, 0), 3)
+        if area>5000:
+            #cv2.drawContours(imgContour, cnt, -1, (255, 0, 0), 3)
             peri = cv2.arcLength(cnt,True)
             approx = cv2.approxPolyDP(cnt,0.02*peri,True)
-            x, y, w, h = cv2.boundingRect(approx)
-    return x+w//2,y
+            if area >maxArea and len(approx) == 4:
+                biggest = approx
+                maxArea = area
+    cv2.drawContours(imgContour, biggest, -1, (255, 0, 0), 20)
+    return biggest
 
-def drawOnCanvas(myPoints,myColorValues):
-    for point in myPoints:
-        cv2.circle(imgResult, (point[0], point[1]), 10, myColorValues[point[2]], cv2.FILLED)
+def reorder (myPoints):
+    myPoints = myPoints.reshape((4,2))
+    myPointsNew = np.zeros((4,1,2),np.int32)
+    add = myPoints.sum(1)
+    #print("add", add)
+    myPointsNew[0] = myPoints[np.argmin(add)]
+    myPointsNew[3] = myPoints[np.argmax(add)]
+    diff = np.diff(myPoints,axis=1)
+    myPointsNew[1]= myPoints[np.argmin(diff)]
+    myPointsNew[2] = myPoints[np.argmax(diff)]
+    #print("NewPoints",myPointsNew)
+    return myPointsNew
 
+def getWarp(img,biggest):
+    biggest = reorder(biggest)
+    pts1 = np.float32(biggest)
+    pts2 = np.float32([[0, 0], [widthImg, 0], [0, heightImg], [widthImg, heightImg]])
+    matrix = cv2.getPerspectiveTransform(pts1, pts2)
+    imgOutput = cv2.warpPerspective(img, matrix, (widthImg, heightImg))
+
+    imgCropped = imgOutput[20:imgOutput.shape[0]-20,20:imgOutput.shape[1]-20]
+    imgCropped = cv2.resize(imgCropped,(widthImg,heightImg))
+
+    return imgCropped
+
+
+def stackImages(scale,imgArray):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range ( 0, rows):
+            for y in range(0, cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank]*rows
+        hor_con = [imageBlank]*rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor= np.hstack(imgArray)
+        ver = hor
+    return ver
 
 while True:
     success, img = cap.read()
-    imgResult = img.copy()
-    newPoints = findColor(img, myColors,myColorValues)
-    if len(newPoints)!=0:
-        for newP in newPoints:
-            myPoints.append(newP)
-    if len(myPoints)!=0:
-        drawOnCanvas(myPoints,myColorValues)
+    img = cv2.resize(img,(widthImg,heightImg))
+    imgContour = img.copy()
 
+    imgThres = preProcessing(img)
+    biggest = getContours(imgThres)
+    if biggest.size !=0:
+        imgWarped=getWarp(img,biggest)
+        # imageArray = ([img,imgThres],
+        #           [imgContour,imgWarped])
+        imageArray = ([imgContour, imgWarped])
+        cv2.imshow("ImageWarped", imgWarped)
+    else:
+        # imageArray = ([img, imgThres],
+        #               [img, img])
+        imageArray = ([imgContour, img])
 
-    cv2.imshow("Result", imgResult)
+    stackedImages = stackImages(0.6,imageArray)
+    cv2.imshow("WorkFlow", stackedImages)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
